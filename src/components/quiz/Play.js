70ab8b5a05d48b842c59e3e0fc9315d11f004a7b
phamphuckhai <34903119+phamphuckhai@users.noreply.github.com>
2019-11-18 21:43:04 +0700

    implement function Random Array

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -8,7 +8,10 @@ import correctNotification from '../../assets/audio/correct.wav';
 import incorrectNotification from '../../assets/audio/incorrect.mp3';
 
 class Play extends Component {
+    
+      
     constructor(props) {
+        shuffle(questions);
         super(props);
         this.state = {
             questions,
@@ -144,5 +147,23 @@ class Play extends Component {
         );
     }
 }
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
 export default Play;
