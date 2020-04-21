b287b492319a83fbaf38afff5c64f40e1c875b79
phamphuckhai <34903119+phamphuckhai@users.noreply.github.com>
2020-04-22 05:38:38 +0700

    create level

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,76 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { IconButton } from '@material-ui/core'
-import {Mood} from '@material-ui/icons';
-import {MoodBad} from '@material-ui/icons';
+import { Mood } from '@material-ui/icons';
+import { MoodBad } from '@material-ui/icons';
 import { Link } from 'react-router-dom'
+import { green } from '@material-ui/core/colors';
 
-const Home = () => (
-    <Fragment>
-        <Helmet><title>Nhà</title></Helmet>
-        <div id="home">
-            <section>
-                <div style={{textAlign: 'center'}}>
-                    <IconButton>
-                      <Mood color="secondary" className="Emoji mod"></Mood>
-                        <MoodBad color="secondary" className="Emoji badmod"></MoodBad>
-                    </IconButton>
+
+class Home extends Component {
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            level: 0,
+            textPlay: 'Chơi'
+        };
+        this.soEzRef = React.createRef();
+        this.ezRef = React.createRef();
+        this.mRef = React.createRef();
+        this.dfRef = React.createRef();
+        this.soDfRef = React.createRef();
+        this.playRef = React.createRef();
+    }
+
+    handleOnClick = () => {
+        this.soEzRef.current.classList.remove('Hiden');
+        this.ezRef.current.classList.remove('Hiden');
+        this.mRef.current.classList.remove('Hiden');
+        this.dfRef.current.classList.remove('Hiden');
+        this.soDfRef.current.classList.remove('Hiden');
+        // this.playRef.current.
+        this.setState({textPlay: 'Vui lòng chọn mức độ chơi! '});
+
+    }
+    render() {
+        return (
+            <Fragment>
+                <Helmet><title>Nhà</title></Helmet>
+                <div id="home">
+                    <section>
+                        <div style={{ textAlign: 'center' }}>
+                            <IconButton>
+                                <Mood color="secondary" className="Emoji mod"></Mood>
+                                <MoodBad color="secondary" className="Emoji badmod"></MoodBad>
+                            </IconButton>
+                        </div>
+                        <h1>Nhận Diện</h1>
+                        <div className="play-button-container">
+                            <ul>
+                                <li>
+                                    <Link className="play-button" onClick={this.handleOnClick} ref={this.playRef}>{this.state.textPlay}</Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="auth-container" >
+                            <Link to="/play/quiz" className="auth-buttons Hiden" id="soEz" ref={this.soEzRef}>Cực Dễ</Link>
+                            <Link to="/play/quiz" className="auth-buttons Hiden" id="ez" ref={this.ezRef}>Dễ</Link>
+                        </div>
+                        <div className="Ali" style={{textAlign: 'center' }} >
+                        <Link to="/play/quiz" className="auth-buttons Hiden" id="me" ref={this.mRef}>Trung bình</Link>
+                        </div>
+                        <div className="auth-container" >
+                            <Link to="/play/quiz" className="auth-buttons Hiden" id="df" ref={this.dfRef}>Khó</Link>
+                            <Link to="/play/quiz" className="auth-buttons Hiden" id="soDf" ref={this.soDfRef}>Cực khó</Link>
+                        </div>
+
+                    </section>
                 </div>
-                <h1>Nhận Diện</h1>
-                <div className = "play-button-container">
-                    <ul>
-                        <li>
-                            <Link className="play-button" to="/play/instructions">Chơi</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="auth-container">
-                    <Link to = "/login" className="auth-buttons" id="login-button">Đăng nhập</Link>
-                    <Link to = "/register" className="auth-buttons" id="signup-button">Đăng kí</Link>
-                </div>
-            </section>
-        </div>
-    </Fragment>
-    
-);
-    
+            </Fragment>
+        );
+    }
+};
+
 export default Home;
